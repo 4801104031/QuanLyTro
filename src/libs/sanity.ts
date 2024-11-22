3e8ef703bce463e843b8226622d37e41a34767c5
Nguyen Khiem <nguyenthienkhiem2912@gmail.com>
2024-11-22 23:59:53 +0700

    Code lần 3

---
+++ src/libs/sanity.ts
@@ -0,0 +1,8 @@
+import { createClient } from "next-sanity";
+const sanityClient =createClient ({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    useCdn: process.env.NODE_ENV==="production",
+    token: ,
+    apiVersion: " 2021-10-21"
+})
